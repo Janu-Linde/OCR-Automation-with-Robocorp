25b0ade6626e6227d809b4fc8d43357b86e21ee6
Janu <janu.linde@gmail.com>
2026-01-11 22:42:43 +0200

    [UPDATE] Adapted Excel spreadsheet generation to work with work items.

--- consumer.py
+++ consumer.py
@@ -1,39 +1,46 @@
 from robocorp.tasks import task
-
+from robocorp import workitems
 from RPA.JSON import JSON
 from RPA.Excel.Files import Files
 
 
 @task
 def generate_report():
-    create_excel_file()
-    
+    process_traffic_data()
 
-def read_json():
-    js = JSON()
-    data = js.load_json_from_file("output/data.json")
-    
-    combined_data = []
-    
-    for file in data:
-        file_data = []
-        file_data.append(file["file_name"])
-        file_data.append(file["company_reg_number"])
-        file_data.append(file["vat_number"])
-        combined_data.append(file_data)
-    return combined_data
+def process_traffic_data():
+    for item in workitems.inputs:
+        traffic_data = item.payload["traffic_data"]
+        name = traffic_data["file_name"]
+        vat = traffic_data["vat_number"]
+        reg = traffic_data["registration_number"]
+
+        if invalid(vat) or invalid(reg):
+            write_to_excel(name, vat, reg)
+            item.fail()
+        else:
+            write_to_excel(name, vat, reg)
+            item.done()
 
 
-def create_excel_file():
+def invalid(number):
+    if number == "No-match-found":
+        return True
+    else:
+        return False
+
+
+def write_to_excel(file_name, vat_num, reg_num):
     excel = Files()
-    data = read_json()
-
-    excel.create_workbook("output/report.xlsx")
-
-    headers = ["File Name", "VAT Number", "Registration Number"]
-    excel.append_rows_to_worksheet([headers], header=False)
     
-    for row in data:
-        excel.append_rows_to_worksheet([row])
+    try:
+        excel.open_workbook("output/report.xlsx")
+        excel.append_rows_to_worksheet([[file_name, vat_num, reg_num]], header=False)
+        excel.save_workbook()
 
-    excel.save_workbook()
+    except FileNotFoundError:
+        excel.create_workbook("output/report.xlsx")
+        headers = ["File Name", "VAT Number", "Registration Number"]
+        excel.append_rows_to_worksheet([headers], header=False)
+        excel.append_rows_to_worksheet([["second", "second", "second"]], header=False)
+        excel.save_workbook()
